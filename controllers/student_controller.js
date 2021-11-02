5b846489a1ae43f859f9a59dfc72c5b298ef0201
yudialfayat <ayudi3521@gmail.com>
2021-11-02 09:31:11 +0700

    delete and getByNim

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -9,6 +9,21 @@ export const getStudent = async (req, res) => {
     }
 };
 
+export const getStudentByNim = async (req, res) => {
+    const nim = req.params.nim;
+    try {
+        const student = await Student.findOne({nim: nim});
+        if (student){
+            res.json(student);
+        }
+        else{
+            res.status(404).json({error: "Student not found"});
+        }
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+};
+
 export const insertStudent = async (req, res) => {
     const student = {
         nim: req.body.nim,
@@ -22,5 +37,21 @@ export const insertStudent = async (req, res) => {
         res.status(200).json(savedStudent);
     } catch (err) {
         res.status(400).json({error:err.message});
+    }
+};
+
+export const deleteStudent = async (req, res) => {
+    const nim = req.params.nim;
+
+    try { 
+        const removedStudent = await Student.findOneAndRemove({nim: nim});
+        if (removedStudent){
+            res.json(removedStudent);
+        }
+        else{
+            res.status(404).json({error: "Student not found"});
+        }
+    } catch (error) {
+        res.json({error: error.message});
     }
 };
--- routes/students_router.js
+++ routes/students_router.js
@@ -1,12 +1,12 @@
 import Express from "express";
 
-import { getStudent, insertStudent } from "../controllers/student_controller.js";
+import { getStudent, getStudentByNim, insertStudent, deleteStudent } from "../controllers/student_controller.js";
 
 const router = Express.Router();
 
 router.get('/', getStudent);
-
+router.get('/:nim', getStudentByNim);
 router.post('/', insertStudent);
-
+router.delete('/:nim', deleteStudent);
 
 export default router;
