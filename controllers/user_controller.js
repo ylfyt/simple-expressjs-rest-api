f2d64e278b6c3a50bdf142dd431de814d2ee091c
yudialfayat <ayudi3521@gmail.com>
2021-11-03 09:39:51 +0700

    jsonwebtoken

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,33 +1,52 @@
 import Joi from '@hapi/joi';
+import jwt from 'jsonwebtoken';
 import Users from '../models/User.js';
 
-
 const schema = Joi.object({
-    username: Joi.string().min(4).required(),
-    password: Joi.string().min(3).required()
+	username: Joi.string().min(4).required(),
+	password: Joi.string().min(3).required(),
 });
 
 export const register = async (req, res) => {
-    const validation = schema.validate(req.body);
-    if (validation.error){
-        return res.status(400).json({error: validation.error.details[0].message});
-    }
+	const validation = schema.validate(req.body);
+	if (validation.error) {
+		return res.status(400).json({ error: validation.error.details[0].message });
+	}
 
-    try {
-        const userExist = await Users.findOne({username: req.body.username});
-        if (userExist){
-            return res.status(400).json({error: 'User is already exist'});
-        }
+	try {
+		const userExist = await Users.findOne({ username: req.body.username });
+		if (userExist) {
+			return res.status(400).json({ error: 'User is already exist' });
+		}
 
-        const newUser = new Users({username: req.body.username, password: req.body.password});
-        const savedUser = await newUser.save();
-        return res.json(savedUser);
-    } catch (error) {
-        return res.status(400).json({error: error.message});
-    }
-    
-}
+		const newUser = new Users({ username: req.body.username, password: req.body.password });
+		const savedUser = await newUser.save();
+		return res.json(savedUser);
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+};
 
-export const login = (req, res) => {
-    res.send('login');
-}
+export const login = async (req, res) => {
+	const validation = schema.validate(req.body);
+	if (validation.error) {
+		return res.status(400).json({ error: validation.error.details[0].message });
+	}
+
+	try {
+		const userExist = await Users.findOne({ username: req.body.username });
+		if (!userExist) {
+			return res.status(400).json({ erorr: 'Username or password is wrong' });
+		}
+
+		if (userExist.password != req.body.password) {
+			return res.status(400).json({ erorr: 'Username or password is wrong' });
+		}
+
+		// Logged in, Make jwt
+		const token = jwt.sign({ id: userExist.id }, process.env.TOKEN_SECRET);
+		return res.json({ token: token });
+	} catch (error) {
+		return res.status(400).json({ error: error.message });
+	}
+};
